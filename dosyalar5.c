8a6555118dceee27dc13ece85d5554b793a8a530
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-05-27 17:37:06 +0300

    Update dosyalar5.c

--- dosyalar5.c
+++ dosyalar5.c
@@ -3,13 +3,12 @@
 // dosyaya bilgi aktarma
 int main() {
 	char karakter[40];
-	FILE *dosya;
-	dosya=fopen("C:\\Users\\TK\\Desktop\\deneme.txt","w");  
-	
 	char bilgi1[20]="Tugba ";
 	char bilgi2[20]="Kirac ";
 	char bilgi3[20]="21";
-	
+	FILE *dosya;
+	dosya=fopen("C:\\Users\\TK\\Desktop\\deneme.txt","w");  
+
 	fputs(bilgi1,dosya);
 	fputs(bilgi2,dosya);
 	fputs(bilgi3,dosya);
